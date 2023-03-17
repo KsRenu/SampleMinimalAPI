1afad5739ead2e80a8e195be188338bc860d9782
Renu Konda <renuks0802@gmail.com>
2023-03-17 22:06:25 +0530

    student details sample minimal api implementation

---
+++ SampleMinimalAPI/Model/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SampleMinimalAPI.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+    }
+}
+
--- SampleMinimalAPI/Program.cs
+++ SampleMinimalAPI/Program.cs
@@ -1,10 +1,16 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using SampleMinimalAPI.Data;
+using SampleMinimalAPI.Model;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Add dbContext
+builder.Services.AddDbContext<MyDataContext>(opt => opt.UseInMemoryDatabase("Student"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,7 +29,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -36,10 +42,53 @@ app.MapGet("/weatherforecast", () =>
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
+//Student APIs
+app.MapPost("/SaveStudent", async (Student student, MyDataContext db) =>
+{
+    db.Students.Add(student);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/save/{student.Id}", student);
+});
+
+app.MapGet("/GetAllStudent", async (MyDataContext db) =>
+    await db.Students.ToListAsync());
+
+app.MapGet("/GetStudentById/{id}", async (int id, MyDataContext db) =>
+    await db.Students.FindAsync(id)
+        is Student student
+            ? Results.Ok(student)
+            : Results.NotFound());
+
+app.MapPut("/UpdateStudents/{id}", async (int id, Student studentinput, MyDataContext db) =>
+{
+    var student = await db.Students.FindAsync(id);
+
+    if (student is null) return Results.NotFound();
+
+    student.Name = studentinput.Name;
+    student.Phone = studentinput.Phone;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/DeleteStudent/{id}", async (int id, MyDataContext db) =>
+{
+    if (await db.Students.FindAsync(id) is Student student)
+    {
+        db.Students.Remove(student);
+        await db.SaveChangesAsync();
+        return Results.Ok(student);
+    }
+
+    return Results.NotFound();
+});
+
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
-
